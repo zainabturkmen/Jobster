4803c25e9fefa412effa43b0a43ed1f3dff8a4ce
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 18:19:39 +0430

    Register page

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "normalize.css";
 import "./index.css";
 import App from "./App.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App tap="home" />);
