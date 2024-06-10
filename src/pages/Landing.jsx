3082e6c180f3ec10a2b2f723acf9a361bae92c9b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 09:46:50 +0430

    Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -15,13 +15,13 @@ const Landing = () => {
           <h1>
             job <span>tracking</span> app
           </h1>
+          <p>
+            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
+            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
+            crucifix heirloom meggings bicycle rights.
+          </p>
+          <button className="btn btn-hero">login/Register</button>
         </div>
-        <p>
-          Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-          fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-          crucifix heirloom meggings bicycle rights.
-        </p>
-        <button className="btn btn-hero">login/Register</button>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
     </Wrapper>
@@ -36,6 +36,21 @@ const Wrapper = styled.main`
     height: var(--nav-height);
     display: flex;
     align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    /* margin-top: -3rem; */
+  }
+  h1 {
+    font-weight: 700;
+    span{
+      color: var(--primary-500);
+    }
+  }
+  p{
+    color: var(--grey-600);
   }
 `;
 
