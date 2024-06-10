784063d88509764a3b616b2f44009950c04d4d22
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 15:09:50 +0430

    setting up routes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import main from "../assets/images/main.svg";
 import Wrapper from "../assets/images/Wrapper/Landing";
 import { Logo } from "../components";
-import {Link} from "reacr-router-dom"
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <Wrapper>
@@ -19,7 +20,9 @@ const Landing = () => {
             fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
             crucifix heirloom meggings bicycle rights.
           </p>
-          <Link to="/register" className="btn btn-hero">login/Register</Link>
+          <Link to="/register" className="btn btn-hero">
+            login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
