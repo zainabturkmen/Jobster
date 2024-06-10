841b5f6a73329c8f7a76e1758efd805c9cc3f578
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 11:53:15 +0430

    exporting pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+import Landing from "./Landing";
+import Error from "./Error";
+import Dashboard from "./Dashboard";
+import Register from "./Register";
+
+export {Landing, Error, Dashboard, Register}
