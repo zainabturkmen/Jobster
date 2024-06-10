eac0ba026ea9bf56add3705fc665b2d71a086468
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 17:36:58 +0430

    Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Logo } from "../components";
 import Wrapper from "../assets/images/Wrapper/RegisterPage";
 
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMemmber: true,
+
+}
+
 function Register() {
+  const [value, setValue] = useState(initialState)
   return <Wrapper>
-    
+
   </Wrapper>
 }
 
