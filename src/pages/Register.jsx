0af4d57a07978159428bb2fd1a18d1e3946c3c71
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 17:41:10 +0430

    Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,20 +3,34 @@ import { useState, useEffect } from "react";
 import { Logo } from "../components";
 import Wrapper from "../assets/images/Wrapper/RegisterPage";
 
-
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMemmber: true,
-
-}
+};
 
 function Register() {
-  const [value, setValue] = useState(initialState)
-  return <Wrapper>
+  const [value, setValue] = useState(initialState);
 
-  </Wrapper>
+  const handleChange = (e) => {
+    // e.preventDefault()
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onsubmit}>
+        <Logo/>
+        <h3>login</h3>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default Register;
