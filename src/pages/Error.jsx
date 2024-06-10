31842e037ccf59359bc93fb803e8525594e815b2
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 15:46:22 +0430

    Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import img from "../assets/images/not-found.svg"
+import Wrapper from '../assets/images/Wrapper/ErrorPage';
 const Error = () => {
-  return <h1>Error page</h1>
+  return <Wrapper className="full-page">
+<div>
+    <img src={img} alt='not found' />
+</div>
+  </Wrapper>
 }
 
 export default Error;
