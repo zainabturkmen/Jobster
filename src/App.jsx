a6e4a23eb838820323bb220f8ea02a0a1284405f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 14:42:24 +0430

    setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import {BrowserRouter, Routes, Route} from "react-router"
 import { useState } from "react";
 import { Landing, Dashboard, Error, Register } from "./pages";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
