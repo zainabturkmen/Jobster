afee6ce2ea637c48cc7a477c11f3044b8eb7ade9
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-10 11:51:12 +0430

    Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const Register = () => {
-  return <h1>Register Page</h1>
+function Register() {
+  return <h1>Register page</h1>;
 }
 
 export default Register;
