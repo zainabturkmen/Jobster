f6b1e9e93cb61ddb7d298155ff94167460fddcd1
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-11 09:31:15 +0430

    redux toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,14 @@ import { createRoot } from "react-dom/client";
 import "normalize.css";
 import "./index.css";
 import App from "./App.jsx";
+import { store } from "./store.jsx";
+import { Provider } from "react-redux";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App tap="home" />);
+root.render(
+  <Provider store={store}>
+    <App tap="home" />
+  </Provider>
+);
