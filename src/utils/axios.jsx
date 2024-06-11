f20e438e04421f725e92606e26e2ef73bddb3173
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-11 17:17:45 +0430

    making request

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-    baseURL: "",
+    baseURL: "https://jobify-prod.herokuapp.com/api/v1/toolkit",
 })
+
+export default customFetch;
